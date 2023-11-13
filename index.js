c370dbc9f7b7c62e3cff4adfcd0a6259f3642375
helisonoliveira <helisonoliveira@hotmail.com>
2023-11-13 19:06:09 -0300

    criando variável nome e validando a variável

---
+++ index.js
@@ -0,0 +1,8 @@
+let nome = "helison"
+
+
+
+//https://horadecodar.com.br/como-saber-se-uma-variavel-e-string-em-javascript/
+if (typeof nome !== "string") {
+  console.log("insira somente letras")
+} else console.log(nome);
