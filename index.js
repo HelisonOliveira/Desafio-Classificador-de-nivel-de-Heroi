849bcc28d5d4c6225002309aabc136ba0d89cce5
helisonoliveira <helisonoliveira@hotmail.com>
2023-11-13 19:54:48 -0300

    criando xp aleatório

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 let nome = "helison"
-
-
+let xp = Math.floor(Math.random() * 10001)//http://devfuria.com.br/javascript/numeros-aleatorios/
+console.log(xp);
 
 //https://horadecodar.com.br/como-saber-se-uma-variavel-e-string-em-javascript/
 if (typeof nome !== "string") {
   console.log("insira somente letras")
 } else console.log(nome);
+
+switch (xp) {
+  case xp : xp <= 1000 
+    console.log("Você tem " + xp + " de xp: seu está no nível Ferro")
+      break;
+  case xp: xp <= 2000  
+    console.log(xp + " - Bronze")
+      break;
+  case xp: xp <= 5000  
+      console.log(xp + " - Prata")
+        break; 
+  
+}
